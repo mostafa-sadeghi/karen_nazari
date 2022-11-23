5082d089992619d79940e69044b54adb068ba395
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-23 16:04:57 +0330

    first commit

---
+++ abann_25.py
@@ -0,0 +1,80 @@
+# انواع داده در پایتون
+# x = 1
+# print(type(1))
+
+#  نوع داده رشته ای
+# x = '1'
+# y = "1"
+# z = "a"
+# r = "ali"
+# print(type(x))
+
+
+# message = "hello"
+# print(message)
+# print("hello")
+
+
+# رشته چند خطی
+
+# print('''hello every body,
+# here is python class.
+# welcome to our class''')
+# print("""hello every body,
+# here is python class.
+# welcome to our class""")
+
+
+# scape chractres
+
+# message = 'he said: "go shopping"'
+# print(message)
+
+# message = "he said : \"go shopping\""
+# print(message)
+
+
+# message = 'he is going to park and he would\'nt to do some thing'
+# print(message)
+# message = '''he is going to park and he would'nt to do some thing'''
+# print(message)
+# message = """he is going to park and he would'nt to do some thing"""
+# print(message)
+
+#  قراردادن مقدار در درون رشته
+# name = "reza"
+# print("my name is %s" % name)
+# name = "reza"
+# family = "mohamadi"
+# print("my name is %s %s" % (name, family))
+
+# exercise:
+# name = "ali"
+# family = "rezaei"
+# age = 12
+# output = "my name is ali rezaei and i have 12 years old"
+
+
+# استفاده از fstring
+
+# name = "ali"
+# family = "rezaei"
+# message = f"my name is {name} {family}"
+# print(message)
+
+
+# x = 1
+# y = 2
+
+# z = x + y
+
+# print(z)
+
+# concate    چسباندن رشته ها
+# x = '1'
+# y = '2'
+# z = x + y
+# print(z)
+# x = '1'
+# z = x * 3
+# print(z)
