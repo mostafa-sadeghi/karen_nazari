dea60dfd5e20370c661bf5278b4fe3d41b14eac9
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-23 16:58:52 +0330

    azar 2

---
+++ azar_2.py
@@ -0,0 +1,60 @@
+# shopping_list = []
+
+# print(type(shopping_list))
+# shopping_list.append("apple")
+# shopping_list.append("banana")
+# print("my shopping list is: ", shopping_list)
+
+# x = shopping_list[0]
+# print("first item of list is : ", x)
+# y = shopping_list[1]
+# print(f'second item of list is : {y}')
+
+
+# numbers = [1, 2, 3, 4, 5]
+# print("my list is :", numbers)
+# numbers.append(6)
+# print("my list is: ", numbers)
+
+# x = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]
+# print(f"sum of list numbers is: {x}")
+
+# چسباندن رشته ها
+
+# string = ['a','b','c','d']
+
+# output = string[0] + string[1] + string[2] + string[3]
+# print("output is : ", output)
+
+# تغییر مقدار لیست
+# string = ['a', 'b', 'c', 'd']
+
+# string[1] = 'fffffff'
+# print("new list is: ", string)
+# حذف از لیست
+# numbers = [1, 2, 3, 4, 5, 6]
+# del numbers[3]
+# print("numbers is :", numbers)
+
+# slicing
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# new_numbers = numbers[3:8]
+# print("new numbers is: ", new_numbers)
+
+# exercise لیستی از کاراکترها
+# chractres = ['a','b','c','d','e','f']
+
+# slice 'c','d','e'
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# even_numbers = numbers[1::2]
+# print("even numbers are: ", even_numbers)
+
+
+# exercise : نمایش اعدا فرد کمتر از 10
+
+numbers = [1, 2, 3, 4]
+characters = ['a', 'b']
+
+my_list = [numbers, characters]
+print(my_list)
