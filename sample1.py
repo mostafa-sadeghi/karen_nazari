640b09c1308615b069f92f837245d7963e3acf7a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-14 16:59:04 +0330

    23azar

---
+++ sample1.py
@@ -0,0 +1,16 @@
+name1 = input('enter a name ')
+name2 = input('enter a name ')
+name3 = input('enter a name ')
+name4 = input('enter a name ')
+
+print(name1 + ' ' + name2 + ' ' + name3 + ' ' + name4)
+print(name1, name2, name3, name4)
+print(f'{name1} {name2} {name3} {name4}')
+names = []
+names.append(name1)
+names.append(name2)
+names.append(name3)
+names.append(name4)
+print(names)
+print(names[0])
+print(names[1])
---
+++ sample2.py
@@ -0,0 +1,23 @@
+import turtle
+s = turtle.Screen()
+s.bgcolor('orange')
+p = turtle.Pen()
+# p.forward(100)
+# p.left(90)
+# p.forward(100)
+# p.left(90)
+# p.forward(100)
+# p.left(90)
+# p.forward(100)
+# p.left(90)
+
+# p.goto(200, 200)
+# p.setheading(0)
+# p.fd(100)
+# p.speed(0)  # p.speed('fastest')
+p.speed(1)  # p.speed('slowest')
+# p.circle(100, 180)
+# p.circle(100)
+
+
+s.exitonclick()
