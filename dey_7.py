316154ab7365f49d3a5c4b72476173f5a2fa66e5
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-28 16:59:37 +0330

    7dey end

---
+++ dey_7.py
@@ -0,0 +1,213 @@
+# import turtle
+# s = turtle.Screen()
+# s.setup(620, 620)
+# s.bgcolor('black')
+# COLORS = ['red', 'green', 'blue', 'yellow', 'purple']
+# p = turtle.Pen()
+# p.pensize(4)
+# p.shape('turtle')
+# p.penup()
+# p.setpos(-90, 30)
+# p.pendown()
+# p.pencolor(COLORS[0])
+# p.forward(200)
+# p.right(144)
+# p.pencolor(COLORS[1])
+# p.forward(200)
+# p.right(144)
+# p.pencolor(COLORS[2])
+# p.forward(200)
+# p.right(144)
+# p.pencolor(COLORS[3])
+# p.forward(200)
+# p.right(144)
+# p.pencolor(COLORS[4])
+# p.forward(200)
+# p.right(144)
+# p.penup()
+# p.setpos(80, -144)
+# p.pencolor('olive')
+# p.write("hello every body", font=("Arial", 12, 'bold'))
+# p.hideturtle()  # p.ht()
+
+
+# s.exitonclick()
+
+
+###############################################
+# animation clock
+# import turtle
+# import time
+# s = turtle.Screen()
+# s.setup(620, 620)
+# s.bgcolor('black')
+
+# p = turtle.Pen()
+# p.pensize(4)
+# p.shape('turtle')
+
+# p.pencolor('red')
+
+# m = 0
+# i = 0
+
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+
+# time.sleep(6)
+
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+# m += 1  # m = m +1
+# p.penup()
+# p.setheading(-30 * i + 60)
+# p.forward(150)
+# p.pendown()
+# p.forward(25)
+# p.penup()
+# p.forward(20)
+# p.write(m)
+# p.home()
+# i = i + 1
+
+
+# s.exitonclick()
+
+
+import turtle
+
+s = turtle.Screen()
+s.bgcolor('black')
+p = turtle.Pen()
+p.pencolor('red')
+p.fillcolor('green')
+p.begin_fill()
+p.circle(20)
+p.end_fill()
+
+s.exitonclick()
+
+
+# exercise 1:
+یک مربع توپر با رنگ آبی بکش
+# exercise 2:
+یک پنجضلعی توپر با رنگ قرمز بکش
+# exercise 3:
+یک شش ضلعی توپر با رنگ آبی بکش
